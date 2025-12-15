cdb69e69de2d7f110ca7dc8f01b1b167e8b7aa26
student <student@172.16.111.8>
2025-12-15 10:26:47 +0530

    Hai iam testclass cse c

---
+++ mydevopspipeline/src/test/java/mydevopspipeline/test.java
@@ -0,0 +1,12 @@
+package mydevopspipeline;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hai CSE C SEC");
+		
+
+	}
+
+}
